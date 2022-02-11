c33cfa8e0a09c89576d3d1aca051c6fc608c6d5d
Vadim <vadicusrad@gmail.com>
2022-02-11 13:22:24 +0300

    Fixed wrong import

--- src/App.js
+++ src/App.js
@@ -7,28 +7,39 @@ import About from "./Components/About/About";
 import BaseLayout from "./Components/BaseLayout/BaseLayout";
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
+	const [cartItems, setCartItems] = useState([]);
 
-  const itemsCount = cartItems.length;
-  return (
-    <div className="App">
-      <Routes>
-        <Route path={"/"} element={<BaseLayout itemsCount={itemsCount} />}>
-          <Route
-            index
-            element={
-              <Content setCartItems={setCartItems} cartItems={cartItems} />
-            }
-          />
-          <Route
-            path={"cart"}
-            element={<Cart setCartItems={setCartItems} cartItems={cartItems} />}
-          />
-          <Route path={"about"} element={<About />} />
-        </Route>
-      </Routes>
-    </div>
-  );
+	const itemsCount = cartItems.length;
+	return (
+		<div className="App">
+			<Routes>
+				<Route
+					path={"/"}
+					element={<BaseLayout itemsCount={itemsCount} />}
+				>
+					<Route
+						index
+						element={
+							<Content
+								setCartItems={setCartItems}
+								cartItems={cartItems}
+							/>
+						}
+					/>
+					<Route
+						path={"cart"}
+						element={
+							<Cart
+								setCartItems={setCartItems}
+								cartItems={cartItems}
+							/>
+						}
+					/>
+					<Route path={"about"} element={<About />} />
+				</Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- src/Components/Aside/Asaide.js
+++ src/Components/Aside/Asaide.js
@@ -3,49 +3,49 @@ import "./Aside.css";
 import CategoriesItem from "./CategoriesItem/CategoriesItem";
 
 function Aside({ setCurrentCategory }) {
-  const CATEGORIES = "https://fakestoreapi.com/products/categories";
+	const CATEGORIES = "https://fakestoreapi.com/products/categories";
 
-  const [categories, setCategories] = useState([]);
+	const [categories, setCategories] = useState([]);
 
-  useEffect(() => {
-    let cleanUpFunc = false;
-    fetch(CATEGORIES)
-      .then((res) => res.json())
-      .then((response) => {
-        if (!cleanUpFunc) {
-          setCategories(response);
-        }
-      })
-      .catch((error) => console.error("oooops!", error.message));
+	useEffect(() => {
+		let cleanUpFunc = false;
+		fetch(CATEGORIES)
+			.then((res) => res.json())
+			.then((response) => {
+				if (!cleanUpFunc) {
+					setCategories(response);
+				}
+			})
+			.catch((error) => console.error("oooops!", error.message));
 
-    return () => (cleanUpFunc = true);
-  }, []);
+		return () => (cleanUpFunc = true);
+	}, []);
 
-  return (
-    <aside className="Content-filters">
-      <div className="Content-filters-categories">
-        <h3>Categories</h3>
-        <ul id="Categories-list">
-          <li
-            onClick={() => setCurrentCategory("")}
-            className="Category-list-item"
-          >
-            all
-          </li>
+	return (
+		<aside className="Content-filters">
+			<div className="Content-filters-categories">
+				<h3>Categories</h3>
+				<ul id="Categories-list">
+					<li
+						onClick={() => setCurrentCategory("")}
+						className="Category-list-item"
+					>
+						all
+					</li>
 
-          {categories.map((item, id) => {
-            return (
-              <CategoriesItem
-                setCurrentCategory={setCurrentCategory}
-                categoryName={item}
-                key={id}
-              />
-            );
-          })}
-        </ul>
-      </div>
-    </aside>
-  );
+					{categories.map((item, id) => {
+						return (
+							<CategoriesItem
+								setCurrentCategory={setCurrentCategory}
+								categoryName={item}
+								key={id}
+							/>
+						);
+					})}
+				</ul>
+			</div>
+		</aside>
+	);
 }
 
 export default Aside;
